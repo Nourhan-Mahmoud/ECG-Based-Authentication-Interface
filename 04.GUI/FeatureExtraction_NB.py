4562801db0fc3ab4745b27f4e680a773a2a06022
Ahmed Mahmoud <nourhanmahmoud623@gmail.com>
2023-05-16 00:27:52 +0300

    mod

--- 04.GUI/FeatureExtraction_NB.py
+++ 04.GUI/FeatureExtraction_NB.py
@@ -7,17 +7,18 @@ from matplotlib import pyplot as plt
 import math
 from scipy.signal import butter, filtfilt
 import pywt
+import pandas as pd
+fs = 1000
+# region reading Data
 
 
-# region reading Data 
 def read_data(path):
-    fs=1000
+    fs = 1000
     patient = wfdb.rdrecord(path, channels=[1])
-    signal = patient.p_signal[: ,0]
-    time = len(signal)/ fs
-    return signal ,time 
-#endregion 
-
+    signal = patient.p_signal[:, 0]
+    time = len(signal) / fs
+    return signal, time, patient
+# endregion
 
 # region Preproccessing
 
@@ -26,19 +27,22 @@ def butter_bandpass_filter(input_signal, low_cutoff, high_cutoff, sampling_rate,
     nyq = 0.5 * sampling_rate
     low = low_cutoff / nyq
     high = high_cutoff / nyq
-    numerator, denominator = butter(order, [low, high], btype='band', output='ba', analog=False, fs=None)
+    numerator, denominator = butter(
+        order, [low, high], btype='band', output='ba', analog=False, fs=None)
     filtered = filtfilt(numerator, denominator, input_signal)
     return filtered
 
-def smoothMAconv(depth,temp, scale): # Moving average by numpy convolution
-    dz = np.diff(depth) 
+
+def smoothMAconv(depth, temp, scale):  # Moving average by numpy convolution
+    dz = np.diff(depth)
     N = int(scale/dz[0])
-    smoothed=np.convolve(temp, np.ones((N,))/N, mode='same') 
+    smoothed = np.convolve(temp, np.ones((N,))/N, mode='same')
     return smoothed
+
 
 def get_onset_offset(X, Y, signal):
     a_norm = math.sqrt((Y[0]-X[0])**2 + (Y[1]-X[1])**2)
-    
+
     a = np.array([[X[0], X[1]], [Y[0], Y[1]]])
 
     c_x = X[0]
@@ -53,12 +57,13 @@ def get_onset_offset(X, Y, signal):
             if c_x >= Y[0]:
                 return max_x
             c_x += 1
-        
+
         c = np.array([[X[0], X[1]], [c_x, signal[int(c_x)]]])
-        
+
         ac_cross = np.cross(a, c)
-        m_cross = (a[0][0]-a[1][0]) * (c[0][1]-c[1][1]) - (a[0][1]-a[1][1]) * (c[0][0]-c[1][0])
-        
+        m_cross = (a[0][0]-a[1][0]) * (c[0][1]-c[1][1]) - \
+            (a[0][1]-a[1][1]) * (c[0][0]-c[1][0])
+
         ac_norm = np.linalg.norm(ac_cross)
         sigma = ac_norm / a_norm
         if X[0] > Y[0]:
@@ -69,14 +74,16 @@ def get_onset_offset(X, Y, signal):
         if sigma > prev_sigma_max:
             prev_sigma_max = sigma
             max_x = int(c_x)
-#endregion
-
+# endregion
 
 # region helper functions
+
+
 def processing(signal):
     # STEPS 1 to 4
     # 1. Bandpass (low pass / high pass)
-    y_lfiltered = butter_bandpass_filter(signal, low_cutoff=1.0, high_cutoff=40.0, sampling_rate=1000, order=2)
+    y_lfiltered = butter_bandpass_filter(
+        signal, low_cutoff=1.0, high_cutoff=40.0, sampling_rate=1000, order=2)
     denoised_signal = y_lfiltered
 
     # 2. Differentiation
@@ -87,12 +94,13 @@ def processing(signal):
 
     # 4. Window smoothing
     n = 40
-    y_lfiltered=np.convolve(y_lfiltered, np.ones((n,))/n, mode='same')
+    y_lfiltered = np.convolve(y_lfiltered, np.ones((n,))/n, mode='same')
 
     # Resize for next processing
     y_lfiltered = y_lfiltered * 1000
     window_smoothed_signal = y_lfiltered.copy()
-    return denoised_signal,y_lfiltered,window_smoothed_signal
+    return denoised_signal, y_lfiltered, window_smoothed_signal
+
 
 def process_signal(denoised_signal, y_lfiltered):
     # Reduce all values close to zero to be zero
@@ -168,12 +176,12 @@ def process_signal(denoised_signal, y_lfiltered):
 
     return qx, qy, sx, sy
 
-def process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal,qx, qy, sx, sy):
+
+def process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal, qx, qy, sx, sy):
     # Thresholding (Get R, any value not the peak between Q and S is set to 0)
     for i in range(len(qx)):
         y_lfiltered[qx[i]:sx[i]][y_lfiltered[qx[i]:sx[i]]
                                  != max(y_lfiltered[qx[i]:sx[i]])] = 0
-
 
     # Remove any peaks that are not Rs
     y_lfiltered[y_lfiltered < max(y_lfiltered) * 0.6] = 0
@@ -208,7 +216,6 @@ def process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal,qx, qy, sx,
     Rx = np.array(Rx)
     Ry = np.array(Ry)
 
-
     # Get QRS onset and offset
     qrs_off_x = []
     qrs_on_x = []
@@ -238,7 +245,7 @@ def extract_p_wave(fs, qrs_on_x, denoised_signal):
     Py = []
     for loc in qrs_on_x:
         start_idx = int(loc - window_size)
-        
+
         px = 0
         py = -5
         for i in range(start_idx, loc):
@@ -285,12 +292,12 @@ def calculate_t_wave(qrs_off_x, denoised_signal):
     for loc in qrs_off_x:
         tx = 0
         ty = 5
-        loc+=10
+        loc += 10
         start_idx = int(loc + window_size)
         for i in range(loc, start_idx):
-            if(i+50<len(denoised_signal)):
+            if(i+50 < len(denoised_signal)):
                 w1 = denoised_signal[i - 50] - denoised_signal[i]
-               
+
                 w2 = denoised_signal[i] - denoised_signal[i + 50]
                 w = w1 * w2
                 if w < ty:
@@ -315,7 +322,7 @@ def calculate_t_onset_offset(Tx, denoised_signal):
         Y = np.array([x - 200, denoised_signal[x - 200]])
         t_on_x.append(get_onset_offset(X, Y, denoised_signal))
         t_on_y.append(denoised_signal[t_on_x[-1]])
-        if(x+150 <len(denoised_signal)):
+        if(x+150 < len(denoised_signal)):
             Y = np.array([x + 200, denoised_signal[x + 200]])
         else:
             Y = np.array([len(denoised_signal)-1, denoised_signal[-1]])
@@ -396,10 +403,12 @@ def extract_non_fiducial_feature(nonFiducial):
     # Only the coefficients of ECG band (1-40) use them as a feature
     non_fiducial_feature = CA5[:41]
 
-    #plt.plot(non_fiducial_feature)
-    #plt.show()
+    # plt.plot(non_fiducial_feature)
+    # plt.show()
 
     return non_fiducial_feature
+
+
 def create_dataframe(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y, Px, Py, p_on_x, p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y):
     data = {
         'qx': qx,
@@ -429,28 +438,29 @@ def create_dataframe(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_
     df = pd.DataFrame(data)
     return df
 
-#endregion
+# endregion
 
 # region Feature Detection
+
+
 def Feature_Detection(signal):
     fs = 1000
-    denoised_signal,y_lfiltered,window_smoothed_signal=processing(signal)
+    denoised_signal, y_lfiltered, window_smoothed_signal = processing(signal)
     time = len(signal)/fs
-    qx, qy, sx, sy=process_signal(denoised_signal, y_lfiltered)
-    qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y =process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal,
-                                                                                  qx, qy, sx, sy)
+    qx, qy, sx, sy = process_signal(denoised_signal, y_lfiltered)
+    qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y = process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal,
+                                                                                   qx, qy, sx, sy)
     Px, Py = extract_p_wave(fs, qrs_on_x, denoised_signal)
-    p_on_x, p_off_x, p_on_y, p_off_y = calculate_p_onset_offset(Px, denoised_signal)
+    p_on_x, p_off_x, p_on_y, p_off_y = calculate_p_onset_offset(
+        Px, denoised_signal)
     Tx, Ty = calculate_t_wave(qrs_off_x, denoised_signal)
-    t_on_x, t_off_x, t_on_y, t_off_y = calculate_t_onset_offset(Tx, denoised_signal)
+    t_on_x, t_off_x, t_on_y, t_off_y = calculate_t_onset_offset(
+        Tx, denoised_signal)
 
     Fiducial_Points = create_dataframe(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y,
-                      Px, Py, p_on_x, p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y)
+                                       Px, Py, p_on_x, p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y)
     nonFiducial = calculate_non_fiducial(Rx, denoised_signal)
     non_fiducial_feature = extract_non_fiducial_feature(nonFiducial)
     #plot_signals_with_t(denoised_signal, y_lfiltered, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y,qx, qy, sx, sy, Rx, Ry, Px, Py, p_on_x, p_on_y, p_off_x, p_off_y,Tx, Ty, t_on_x, t_on_y, t_off_x, t_off_y, time, fs)
-    return Fiducial_Points ,non_fiducial_feature
+    return Fiducial_Points, non_fiducial_feature
 # endregion
-
-
-
--- 04.GUI/pages/Classification.py
+++ 04.GUI/pages/Classification.py
@@ -1,3 +1,6 @@
+import pickle
+import pathlib
+import shutil
 import warnings
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -7,36 +10,62 @@ import streamlit as st
 import requests
 import seaborn as sns
 from streamlit_lottie import st_lottie
+import os
+from FeatureExtraction_NB import read_data, Feature_Detection
+import wfdb
 
 st.set_page_config(page_title='ECG Based Authentication Interface',
                    page_icon=':star:', layout="wide")
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.write("## Classification")
+
+# region Load Model
+fid_svm = pickle.load(open('HCI_SVM_F.pkl', 'rb'))
+# endregion
 
 # region Load Data
 uploaded_files = st.file_uploader(
     "Choose Signal: ", accept_multiple_files=True)
+
+os.makedirs("temp", exist_ok=True)
+signal_name = ""
+signal = ""
+time = ""
 for uploaded_file in uploaded_files:
-    data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
+    signal_name = uploaded_file.name.split(".")[0]
+    with open(os.path.join("temp", uploaded_file.name), "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    # st.write("Done Reading file:", uploaded_file.name)
+if signal_name != "":
+    signal, time, record = read_data("temp/"+signal_name)
+    # st.write("Signal Shape:", signal.shape)
+    fig = wfdb.plot_wfdb(record=record, figsize=(200, 20), title='Record')
+    st.pyplot(fig)
+    shutil.rmtree("temp")
+    st.write("Done Reading files!")
+
 # endregion
 
-# region Pre-processing
-
-# endregion
-
-# region Segmentation
-
-# endregion
 
 # region Feature Extraction
-
-# endregion
-
-# region Load Model
-
+fiducial_points = ""
+non_fiducial_points = ""
+if signal != "" and time != "":
+    fiducial_points, non_fiducial_points = Feature_Detection(signal)
 # endregion
 
 # region Classification
-
+if st.button("Predict"):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("With Fiducial Features")
+        st.write("---")
+        prediction = fid_svm.predict(fiducial_points)
+        values, counts = np.unique(prediction, return_counts=True)
+        st.write("Person ID:", int(values[counts.argmax()]))
+        st.write("Prediction For Each Segment:")
+        st.table(prediction)
+    with col2:
+        st.subheader("With Non Fiducial Features")
+        st.write("---")
 # endregion
