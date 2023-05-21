7615074feabbb09d0c816e1fb416fb90f85ad0b6
Ahmed Mahmoud <nourhanmahmoud623@gmail.com>
2023-05-21 03:12:45 +0300

    Mod

--- 04.GUI/feature_extraction.py
+++ 04.GUI/feature_extraction.py
@@ -1,26 +1,45 @@
-import matplotlib.pyplot as plt
+
+from scipy.signal import butter, filtfilt
 import wfdb
 from scipy import signal
 import scipy
 import numpy as np
 from matplotlib import pyplot as plt
 import math
-from scipy.signal import butter, filtfilt
-import pywt
 import pandas as pd
+import pywt
+from statsmodels.graphics import tsaplots
+import statsmodels.api as sm
+
+
+# # Read Record
+
+
 fs = 1000
-# region reading Data
+# best start = 1300, best end = 3200
+patient_1 = wfdb.rdrecord('..//01.Dataset/104/s0306lre', channels=[1])
+patient_2 = wfdb.rdrecord('..//01.Dataset/117/s0291lre', channels=[1])
+patient_3 = wfdb.rdrecord('..//01.Dataset/122/s0312lre', channels=[1])
+patient_4 = wfdb.rdrecord('..//01.Dataset/166/s0275lre', channels=[1])
+patient_5 = wfdb.rdrecord('..//01.Dataset/173/s0305lre', channels=[1])
+patient_6 = wfdb.rdrecord('..//01.Dataset/182/s0308lre', channels=[1])
+patient_7 = wfdb.rdrecord('..//01.Dataset/234/s0460_re', channels=[1])
+patient_8 = wfdb.rdrecord('..//01.Dataset/238/s0466_re', channels=[1])
+patient_9 = wfdb.rdrecord('..//01.Dataset/255/s0491_re', channels=[1])
+patient_10 = wfdb.rdrecord('..//01.Dataset/252/s0487_re', channels=[1])
 
+signal_1 = patient_1.p_signal[:, 0]
+signal_2 = patient_2.p_signal[:, 0]
+signal_3 = patient_3.p_signal[:, 0]
+signal_4 = patient_4.p_signal[:, 0]
+signal_5 = patient_5.p_signal[:, 0]
+signal_6 = patient_6.p_signal[:, 0]
+signal_7 = patient_7.p_signal[:, 0]
+signal_8 = patient_8.p_signal[:, 0]
+signal_9 = patient_9.p_signal[:, 0]
+signal_10 = patient_10.p_signal[:, 0]
 
-def read_data(path):
-    fs = 1000
-    patient = wfdb.rdrecord(path, channels=[1])
-    signal = patient.p_signal[:, 0]
-    time = len(signal) / fs
-    return signal, time, patient
-# endregion
-
-# region Preproccessing
+time = len(signal_1) / fs
 
 
 def butter_bandpass_filter(input_signal, low_cutoff, high_cutoff, sampling_rate, order):
@@ -74,9 +93,6 @@ def get_onset_offset(X, Y, signal):
         if sigma > prev_sigma_max:
             prev_sigma_max = sigma
             max_x = int(c_x)
-# endregion
-
-# region helper functions
 
 
 def processing(signal):
@@ -179,12 +195,13 @@ def process_signal(denoised_signal, y_lfiltered):
 
 def process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal, qx, qy, sx, sy):
     # Thresholding (Get R, any value not the peak between Q and S is set to 0)
+    print(len(denoised_signal), len(y_lfiltered), len(window_smoothed_signal))
     for i in range(len(qx)):
         y_lfiltered[qx[i]:sx[i]][y_lfiltered[qx[i]:sx[i]]
                                  != max(y_lfiltered[qx[i]:sx[i]])] = 0
 
     # Remove any peaks that are not Rs
-    y_lfiltered[y_lfiltered < max(y_lfiltered) * 0.6] = 0
+    y_lfiltered[y_lfiltered < max(y_lfiltered)*0.65] = 0
 
     # Retrieve R
     Rx = []
@@ -226,8 +243,11 @@ def process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal, qx, qy, sx
         Y = np.array([x - 300, window_smoothed_signal[x - 300]])
         qrs_on_x.append(get_onset_offset(X, Y, window_smoothed_signal))
         qrs_on_y.append(denoised_signal[qrs_on_x[-1]])
-
-        Y = np.array([x + 200, window_smoothed_signal[x + 200]])
+        if(x+200 < len(window_smoothed_signal)):
+            Y = np.array([x + 200, window_smoothed_signal[x + 200]])
+        else:
+            Y = np.array([len(window_smoothed_signal)-1,
+                         window_smoothed_signal[len(window_smoothed_signal)-1]])
         qrs_off_x.append(get_onset_offset(X, Y, window_smoothed_signal))
         qrs_off_y.append(denoised_signal[qrs_off_x[-1]])
 
@@ -238,12 +258,52 @@ def process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal, qx, qy, sx
     return qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y
 
 
+def plot_signals(time, fs, denoised_signal, window_smoothed_signal, qx, qy, sx, sy):
+    # Plot
+    ts = np.arange(0, time, 1.0 / fs)  # time vector
+
+    fig = plt.figure(figsize=[15, 10.4])
+    gs = fig.add_gridspec(2, hspace=0)
+    axs = gs.subplots(sharex=True, sharey=False)
+    fig.suptitle("Signals")
+    axs[0].plot(ts, denoised_signal, alpha=0.6, lw=1, label="Raw signal")
+    axs[1].plot(ts, window_smoothed_signal, alpha=0.6,
+                lw=1, label="SciPy lfilter")
+
+    axs[0].scatter(qx / fs, qy, color="red", s=7)
+    axs[0].scatter(sx / fs, sy, color="green", s=7)
+    #axs[0].scatter(Rx / fs, Ry, color="blue", s=7)
+
+    plt.show()
+
+
+def plot_qrs_results(time, fs, denoised_signal, y_lfiltered, qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y):
+    # Plot
+    ts = np.arange(0, time, 1.0 / fs)  # time vector
+
+    fig = plt.figure(figsize=[15, 10.4])
+    gs = fig.add_gridspec(2, hspace=0)
+    axs = gs.subplots(sharex=True, sharey=False)
+    fig.suptitle("Signals")
+    axs[0].plot(ts, denoised_signal, alpha=0.6, lw=1, label="Raw signal")
+    axs[1].plot(ts, y_lfiltered, alpha=0.6, lw=1, label="SciPy lfilter")
+
+    axs[0].scatter(qrs_on_x / fs, qrs_on_y, color="yellow", s=7)
+    axs[0].scatter(qrs_off_x / fs, qrs_off_y, color="lime", s=7)
+    axs[0].scatter(qx / fs, qy, color="red", s=7)
+    axs[0].scatter(sx / fs, sy, color="green", s=7)
+    axs[0].scatter(Rx / fs, Ry, color="blue", s=7)
+
+    plt.show()
+
+
 def extract_p_wave(fs, qrs_on_x, denoised_signal):
     # Get P wave
     window_size = int(fs * 0.2)  # 200 ms window
     Px = []
     Py = []
     for loc in qrs_on_x:
+        loc = qrs_on_x[0]  # QRS onset
         start_idx = int(loc - window_size)
 
         px = 0
@@ -285,14 +345,13 @@ def calculate_p_onset_offset(Px, denoised_signal):
 
 
 def calculate_t_wave(qrs_off_x, denoised_signal):
-    window_size = int(fs * 0.5)  # 400 ms window
+    window_size = int(fs * 0.4)  # 400 ms window
     Tx = []
     Ty = []
 
     for loc in qrs_off_x:
         tx = 0
         ty = 5
-        loc += 10
         start_idx = int(loc + window_size)
         for i in range(loc, start_idx):
             if(i+50 < len(denoised_signal)):
@@ -323,7 +382,7 @@ def calculate_t_onset_offset(Tx, denoised_signal):
         t_on_x.append(get_onset_offset(X, Y, denoised_signal))
         t_on_y.append(denoised_signal[t_on_x[-1]])
         if(x+150 < len(denoised_signal)):
-            Y = np.array([x + 200, denoised_signal[x + 200]])
+            Y = np.array([x + 150, denoised_signal[x + 150]])
         else:
             Y = np.array([len(denoised_signal)-1, denoised_signal[-1]])
         t_off_x.append(get_onset_offset(X, Y, denoised_signal))
@@ -366,49 +425,6 @@ def plot_signals_with_t(denoised_signal, y_lfiltered, qrs_on_x, qrs_on_y, qrs_of
     plt.show()
 
 
-def calculate_non_fiducial(Rx, denoised_signal):
-    RR_previous = Rx[1] - Rx[0]
-    RR_next = Rx[2] - Rx[1]
-
-    nonFiducial = []
-
-    after_Rpeak = int(2/3 * ((RR_previous + RR_next) / 2))
-
-    for i in range(int(Rx[1]), int(Rx[1]) + after_Rpeak):
-        nonFiducial.append(denoised_signal[i])
-
-    Before_Rpeak = int(1/3 * ((RR_previous + RR_next) / 2))
-
-    for i in range(Before_Rpeak, int(Rx[1])):
-        nonFiducial.append(denoised_signal[i])
-   # plt.plot(nonFiducial)
-    return nonFiducial
-
-
-def extract_non_fiducial_feature(nonFiducial):
-    # Define the mother wavelet
-    wavelet = pywt.Wavelet('db4')
-
-    # Define the number of levels for decomposition
-    decomp_levels = 5
-
-    # Define the list of ECG signals
-    ecg_segments = np.array(nonFiducial)  # Example data
-
-    # Decompose the signal
-    decomp = pywt.wavedec(ecg_segments, wavelet, level=decomp_levels)
-
-    CA5, CD5, CD4, CD3, CD2, CD1 = decomp
-
-    # Only the coefficients of ECG band (1-40) use them as a feature
-    non_fiducial_feature = CA5[:41]
-
-    # plt.plot(non_fiducial_feature)
-    # plt.show()
-
-    return non_fiducial_feature
-
-
 def create_dataframe(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y, Px, Py, p_on_x, p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y):
     data = {
         'qx': qx,
@@ -438,16 +454,43 @@ def create_dataframe(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_
     df = pd.DataFrame(data)
     return df
 
-# endregion
 
-# region Feature Detection
+def print_list_lengths(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y, Px, Py, p_on_x, p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y):
+    lists = {
+        'qx': qx,
+        'qy': qy,
+        'sx': sx,
+        'sy': sy,
+        'Rx': Rx,
+        'Ry': Ry,
+        'qrs_on_x': qrs_on_x,
+        'qrs_on_y': qrs_on_y,
+        'qrs_off_x': qrs_off_x,
+        'qrs_off_y': qrs_off_y,
+        'Px': Px,
+        'Py': Py,
+        'p_on_x': p_on_x,
+        'p_off_x': p_off_x,
+        'p_on_y': p_on_y,
+        'p_off_y': p_off_y,
+        'Tx': Tx,
+        'Ty': Ty,
+        't_on_x': t_on_x,
+        't_off_x': t_off_x,
+        't_on_y': t_on_y,
+        't_off_y': t_off_y
+    }
+
+    for name, lst in lists.items():
+        print(f"Length of {name}: {len(lst)}")
 
 
-def Feature_Detection(signal):
-    fs = 1000
-    denoised_signal, y_lfiltered, window_smoothed_signal = processing(signal)
+def Fiducial_Points_Detection(signal):
+    # print(type(signal))
+    denoised_signal, y_lfiltered, window_smoothed_signal = signal
     time = len(signal)/fs
     qx, qy, sx, sy = process_signal(denoised_signal, y_lfiltered)
+    #plot_signals(time, fs, denoised_signal, window_smoothed_signal, qx, qy, sx, sy)
     qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y = process_qrs(denoised_signal, y_lfiltered, window_smoothed_signal,
                                                                                    qx, qy, sx, sy)
     Px, Py = extract_p_wave(fs, qrs_on_x, denoised_signal)
@@ -456,11 +499,89 @@ def Feature_Detection(signal):
     Tx, Ty = calculate_t_wave(qrs_off_x, denoised_signal)
     t_on_x, t_off_x, t_on_y, t_off_y = calculate_t_onset_offset(
         Tx, denoised_signal)
-
+    # print_list_lengths(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y, Px, Py, p_on_x,
+    #                    p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y)
+    # print(min(Ry))
+    #plot_qrs_results(time, fs, denoised_signal, y_lfiltered, qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y)
     Fiducial_Points = create_dataframe(qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y,
                                        Px, Py, p_on_x, p_off_x, p_on_y, p_off_y, Tx, Ty, t_on_x, t_off_x, t_on_y, t_off_y)
-    nonFiducial = calculate_non_fiducial(Rx, denoised_signal)
-    non_fiducial_feature = extract_non_fiducial_feature(nonFiducial)
-    #plot_signals_with_t(denoised_signal, y_lfiltered, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y,qx, qy, sx, sy, Rx, Ry, Px, Py, p_on_x, p_on_y, p_off_x, p_off_y,Tx, Ty, t_on_x, t_on_y, t_off_x, t_off_y, time, fs)
-    return Fiducial_Points, non_fiducial_feature
+    return Fiducial_Points
+
+# region Bouns Feature Detection
+
+
+def get_Rs(signal):
+    denoised_signal, y_lfiltered, window_smoothed_signal = signal
+    qx, qy, sx, sy = process_signal(denoised_signal, y_lfiltered)
+    qx, qy, sx, sy, Rx, Ry, qrs_on_x, qrs_on_y, qrs_off_x, qrs_off_y = process_qrs(
+        denoised_signal, y_lfiltered, window_smoothed_signal, qx, qy, sx, sy)
+    return denoised_signal, Rx
+
+
+def non_fiducial_features_bonus(signal):
+    denoised_signal, Rx = get_Rs(signal)
+    nonFiducials_for_Full_signal_1 = []
+
+    for i in range(1, len(Rx)-1):
+        RR_previous = Rx[i]-Rx[i-1]
+        RR_next = Rx[i+1]-Rx[i]
+
+        nonFiducial = []
+
+        after_Rpeak = int(2/3*((RR_previous+RR_next)/2))
+
+        for x in range(int(Rx[i]), int(Rx[i])+after_Rpeak):
+            nonFiducial.append(denoised_signal[x])
+
+        Before_Rpeak = int(1/3*((RR_previous+RR_next)/2))
+
+        for j in range(Before_Rpeak+int(Rx[i-1]), int(Rx[i])):
+            nonFiducial.append(denoised_signal[j])
+
+        nonFiducials_for_Full_signal_1.append(nonFiducial)
+
+    list_of_non_fiducial_features_1 = []
+    for i in range(len(nonFiducials_for_Full_signal_1)):
+        # Define the mother wavelet
+        wavelet = pywt.Wavelet('db4')
+
+        # Define the number of levels for decomposition
+        decomp_levels = 5
+
+        # Define the list of ECG signals
+        ecg_segments = np.array(
+            nonFiducials_for_Full_signal_1[i])  # Example data
+
+        # Decompose the signal
+        decomp = pywt.wavedec(ecg_segments, wavelet, level=decomp_levels)
+
+        CA5, CD5, CD4, CD3, CD2, CD1 = decomp
+
+        # only the coefficients of ECG band (1-40) use them as a feature
+        non_fiducial_feature = CA5[:40]
+        non_fiducial_feature = list(non_fiducial_feature)
+        if len(non_fiducial_feature) < 40:
+            for i in range(40-len(non_fiducial_feature)):
+                non_fiducial_feature.append(0)
+        list_of_non_fiducial_features_1.append(np.array(non_fiducial_feature))
+    return list_of_non_fiducial_features_1
+
 # endregion
+
+
+# apply AC and DCT
+def nonFiducial(signal):
+
+    signal = signal[0]
+
+    Auto_corr = sm.tsa.acf(signal, nlags=len(signal))
+
+    s1 = Auto_corr[:1100]
+
+    DcT = scipy.fftpack.dct(s1, type=2)
+
+    # take only non zero signal
+    dct = DcT[:80]
+
+    components = [signal, Auto_corr, s1, DcT, dct]
+    return components[4]
--- 04.GUI/pages/Classification.py
+++ 04.GUI/pages/Classification.py
@@ -11,8 +11,9 @@ import requests
 import seaborn as sns
 from streamlit_lottie import st_lottie
 import os
-from FeatureExtraction_NB import read_data, Feature_Detection
 import wfdb
+from final_project import *
+from feature_extraction import *
 
 st.set_page_config(page_title='ECG Based Authentication Interface',
                    page_icon=':star:', layout="wide")
@@ -20,8 +21,23 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.write("## Classification")
 
 # region Load Model
-fid_svm = pickle.load(open('HCI_SVM_F.pkl', 'rb'))
+non_fid_model = pickle.load(open('random_forest_classifier_nonFid.pkl', 'rb'))
+non_fid_bonus_model = pickle.load(
+    open('random_forest_classifier_nonFidBonus.pkl', 'rb'))
+fid_model = pickle.load(open('random_forest_classifier_Fid.pkl', 'rb'))
 # endregion
+
+# region reading Data from Function
+
+
+def read_data(path):
+    fs = 1000
+    patient = wfdb.rdrecord(path, channels=[1])
+    signal = patient.p_signal[:, 0]
+    time = len(signal) / fs
+    return signal, time, patient
+# endregion
+
 
 # region Load Data
 uploaded_files = st.file_uploader(
@@ -45,27 +61,82 @@ if signal_name != "":
     st.write("Done Reading files!")
 
 # endregion
+methods = {'Fiducial': 0, 'Non Fiducial': 1, 'Non Fiducial Bonus': 2}
+selected_feature_extraction_method = st.radio('Select Feature Extraction Method: ', [
+    "Fiducial", "Non Fiducial", "Non Fiducial Bonus"])
+index = methods[selected_feature_extraction_method]
+st.write("Selected Feature Extraction Method: ",
+         selected_feature_extraction_method)
 
 
-# region Feature Extraction
-fiducial_points = ""
-non_fiducial_points = ""
+input_signal = None
+# region preprocessing
 if signal != "" and time != "":
-    fiducial_points, non_fiducial_points = Feature_Detection(signal)
+    input_signal = processing(signal)
 # endregion
+
+
+def get_features(signal, index):
+    if index == 0:
+        return Fiducial_Features([signal])
+    elif index == 1:
+        return non_fiducial_features([signal])
+    elif index == 2:
+        return non_fiducial_features_bonus_preprocessing([signal])
+
 
 # region Classification
 if st.button("Predict"):
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader("With Fiducial Features")
-        st.write("---")
-        prediction = fid_svm.predict(fiducial_points)
-        values, counts = np.unique(prediction, return_counts=True)
-        st.write("Person ID:", int(values[counts.argmax()]))
-        st.write("Prediction For Each Segment:")
-        st.table(prediction)
-    with col2:
-        st.subheader("With Non Fiducial Features")
-        st.write("---")
+    if index == 0:
+        features, values = get_features(input_signal, index)
+        values = np.array(values).reshape(-1, 23)
+        prediction = fid_model.predict_proba(values[:, :22])
+        threshold_percentage = 0.95
+        flag = 0
+        for i in range(0, len(prediction)):
+            for subject_id, percentage in enumerate(prediction[i]):
+
+                if percentage >= threshold_percentage:
+                    st.write(
+                        f"Identified as subject {subject_id+1} with {percentage*100}% certainty.")
+                    flag = 1
+            if flag == 1:
+                break
+
+        if flag == 0:
+            st.write("subject is undefind")
+    elif index == 1:
+        features, values = get_features(input_signal, index)
+        values = np.array(values).reshape(1, 81)
+        prediction = non_fid_model.predict_proba(values[:, :80])
+
+        threshold_percentage = 0.5
+        flag = 0
+        for i in range(0, len(prediction)):
+            for subject_id, percentage in enumerate(prediction[i]):
+
+                if percentage >= threshold_percentage:
+                    st.write(
+                        f"Identified as subject {subject_id+1} with {percentage*100}% certainty.")
+                    flag = 1
+        if flag == 0:
+            st.write("subject is undefind")
+
+    elif index == 2:
+        features, values = get_features(input_signal, index)
+        values = np.array(values).reshape(-1, 41)
+        prediction = non_fid_bonus_model.predict_proba(values[:, :40])
+        threshold_percentage = 0.95
+        flag = 0
+        for i in range(0, len(prediction)):
+            for subject_id, percentage in enumerate(prediction[i]):
+
+                if percentage >= threshold_percentage:
+                    st.write(
+                        f"Identified as subject {subject_id+1} with {percentage*100}% certainty.")
+                    flag = 1
+            if flag == 1:
+                break
+        if flag == 0:
+            st.write("subject is undefind")
 # endregion
